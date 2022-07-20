721735dbaf3721399aefd23d0554393bab0abd4d
Konstantin Lukyanenok <loukianen@gmail.com>
2022-07-20 18:19:46 +0300

    add change gameState and warning to Start component

--- src/components/start/start.tsx
+++ src/components/start/start.tsx
@@ -1,5 +1,9 @@
 import { Dispatch, SetStateAction } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
+import { getGameState } from '../../store/game-state-process/selectors';
+import { setGameState } from '../../store/game-state-process/game-state-process';
+import { GameStates } from '../../const';
 
 type StartPropsType = {
   onSetWarningText: Dispatch<SetStateAction<string | string[]>>,
@@ -9,16 +13,50 @@ type StartPropsType = {
 const Start = (props: StartPropsType) => {
   const { t } = useTranslation();
   const { onSetWarningText, onSetApprovedAction } = props;
+  const dispatch = useAppDispatch();
+  const gameState = useAppSelector(getGameState);
 
-  const handleClick = () => {
-    onSetWarningText([t('alert.restart'), t('alert.areYouSureToContinue')]);
-    onSetApprovedAction(() => () => {
-      console.log('Restart game');
-    });
+  const isShouldShowWarning = gameState === GameStates.SettingFleet || gameState === GameStates.Started;
+
+  const getButtonLabel = () => {
+    switch (gameState) {
+      case GameStates.NotStarted:
+        return t('ui.navStart');
+      case GameStates.SettingFleet:
+        return t('ui.startBattle');
+      default:
+        return t('ui.newGame');
+    };
   };
 
+  const showWarning = (text: string | string[], approvedAction: Dispatch<SetStateAction<() => void>>) => {
+    onSetWarningText(text);
+    onSetApprovedAction(() => approvedAction);
+  };
+  
+  const handleClick = () => {
+    if (gameState === GameStates.SettingFleet) {
+      showWarning('Are you sure that the fleet arranged?', () => dispatch(setGameState(GameStates.Started)));
+    } else if(gameState === GameStates.Started) {
+      showWarning([t('alert.restart'), t('alert.areYouSureToContinue')], () => dispatch(setGameState(GameStates.SettingFleet)));
+    } else {
+      dispatch(setGameState(GameStates.SettingFleet));
+    }
+  };
+
+  const buttonLabel = getButtonLabel();
+
   return (
-    <button type="button" className="btn btn-light nav-btn" data-bs-toggle="modal" data-bs-target="#warningModal" data-testid="startComponent" onClick={handleClick}>{t('ui.navStart')}</button>
+    <button
+      type="button"
+      className="btn btn-light nav-btn"
+      data-bs-toggle={isShouldShowWarning ? 'modal' : ''}
+      data-bs-target="#warningModal"
+      data-testid="startComponent"
+      onClick={handleClick}
+    >
+      {buttonLabel}
+    </button>
   );
 };
 
